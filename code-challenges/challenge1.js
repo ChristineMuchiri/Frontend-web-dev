bae6144adf653b1981d4e3b91c4bead503b3f35b
ChristineMuchiri <tinniemuchiri@gmail.com>
2023-10-29 14:51:25 +0300

    solutions

---
+++ code-challenges/challenge1.js
@@ -0,0 +1,19 @@
+const myBoolean = true;
+
+const myString = "hello world";
+
+const firstNumber = 20;
+
+let secondNumber = 40;
+secondNumber = 80;
+
+const myArray = [myBoolean, myString];
+
+const myObject = {firstProperty:myArray, sumProperty:Number(firstNumber + secondnumber)};
+
+console.log(myObject);
+
+console.log(myObject.sumProperty);
+
+console.log(myObject.firstProperty[1];
+

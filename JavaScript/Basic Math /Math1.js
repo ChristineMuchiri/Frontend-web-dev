0f08df2559c05b928d0e33b387d95378836ad486
ChristineMuchiri <122861795+ChristineMuchiri@users.noreply.github.com>
2023-11-01 22:49:20 +0300

    math operations

---
+++ JavaScript/Basic Math /Math1.js
@@ -0,0 +1,15 @@
+let finalResult;
+
+let evenOddResult;
+
+
+let variable1 = 12;
+let variable2 = 12;
+let variable3 = 27;
+let variable4 = 25;
+
+const sum = variable1 + variable2;
+const sub = variable3 - variable4;
+
+finalResult = sum * sub;
+evenOddResult = finalResult % 2;

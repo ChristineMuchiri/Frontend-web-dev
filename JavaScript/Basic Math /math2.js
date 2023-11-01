ed7ff070755bf637a14ae5c1e828375c2a1a881a
ChristineMuchiri <122861795+ChristineMuchiri@users.noreply.github.com>
2023-11-01 22:55:22 +0300

    math2

---
+++ JavaScript/Basic Math /math2.js
@@ -0,0 +1,7 @@
+let result = 7 + 13 / 9 + 7;
+let result2 = 100 / 2 * 6;
+result = result * result2;
+const finalResult = result.toFixed(2);
+
+typeof finalResult;
+const finalNumber = Number(finalResult);

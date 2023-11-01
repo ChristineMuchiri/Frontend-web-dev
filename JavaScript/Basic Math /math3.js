a42490f23b6dbf76df5c1b9a7b853d6876c8f62a
ChristineMuchiri <122861795+ChristineMuchiri@users.noreply.github.com>
2023-11-01 23:01:23 +0300

    comparison

---
+++ JavaScript/Basic Math /math3.js
@@ -0,0 +1,15 @@
+
+// Statement 1: The elephant weighs less than the mouse
+const eleWeight = 1000;
+const mouseWeight = 2;
+const weightComparison = eleWeight > mouseWeight;
+
+// Statement 2: The Ostrich is taller than the duck
+const ostrichHeight = 2;
+const duckHeight = 0.3;
+const heightComparison = ostrichHeight > duckHeight;
+
+// Statement 3: The two passwords match
+const pwd1 = 'stromboli';
+const pwd2 = 'stROmBoLi';
+const pwdMatch = pwd1 !== pwd2

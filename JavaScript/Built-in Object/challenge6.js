de2acb60efe83c2a3944c1210aa2ed64ad72fcb7
ChristineMuchiri <122861795+ChristineMuchiri@users.noreply.github.com>
2023-11-14 10:02:46 +0300

    challenge6.js

---
+++ JavaScript/Built-in Object/challenge6.js
@@ -0,0 +1,9 @@
+function isToday(date){
+  const today = new Date();
+  
+  return (
+      date.getFullYear() === today.getFullYear() && 
+      date.getMonth() === today.getMonth() && 
+      date.getDate() === today.getDate()
+);
+}

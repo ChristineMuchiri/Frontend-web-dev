dacebb3c5d7d17c1a3def9421b10c813e5a089e5
ChristineMuchiri <122861795+ChristineMuchiri@users.noreply.github.com>
2023-11-30 14:23:41 +0300

    arrays.js

---
+++ JavaScript/Basic Data Structures/arrays.js
@@ -0,0 +1,6 @@
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+let newVariable = 'me';
+myArray[1] = newVariable;
+// Only change code above this line
+console.log(myArray);
